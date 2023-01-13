f00a58016733903519e4fac1ae125ab82632bb17
Cam <cdang1234@gmail.com>
2023-01-13 17:55:43 -0500

    Cam Commit 2

--- Assets/testScriptCam.cs
+++ Assets/testScriptCam.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testScript : MonoBehaviour
+public class testScriptCam : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        // Poop Dollar!
-        int x = 5;
+        
     }
 
     // Update is called once per frame
